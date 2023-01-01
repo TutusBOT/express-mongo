195394023eb8464e618c02918b4177da5897217d
TutusBOT <barte77pl@gmail.com>
2023-01-01 23:33:50 +0100

    add auth0 as authentication method

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
-import User from "./models/User";
+import { auth } from "express-openid-connect";
+import dotenv from "dotenv";
+import { router } from "./routes";
+
+dotenv.config();
+
+const config = {
+	authRequired: false,
+	auth0Logout: true,
+	secret: process.env.SECRET,
+	baseURL: process.env.BASEURL,
+	clientID: process.env.CLIENTID,
+	issuerBaseURL: process.env.ISSUER,
+};
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(auth(config));
+app.use("/", router);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test");
 
 const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
 db.on("open", () => console.log("Connected to database"));
-
-app.get("/", (req, res) => {
-	res.send("Hello world");
-});
 
 app.listen(process.env.PORT || 8080, () => {
 	console.log(`Listening on port ${process.env.PORT || 8080}`);
